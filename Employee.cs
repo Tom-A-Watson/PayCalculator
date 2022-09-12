e5470d5827d1a296145e90ec71a6fdfcb0acbb6a
SigherWolf <thomasw2701@gmail.com>
2022-09-12 14:59:40 +0100

    Created Employee class and added necessary fields. Also created methods for calculating an Employee's total annual pay and hourly rate

---
+++ Employee.cs
@@ -0,0 +1,26 @@
+﻿namespace PayCalculator
+{
+    public class Employee
+    {
+        string name;
+        double annualSalary;
+        double annualBonus;
+
+        public Employee(string name, double annualSalary, double annualBonus)
+        {
+            this.name = name;
+            this.annualSalary = annualSalary;
+            this.annualBonus = annualBonus;
+        }
+
+        public double TotalAnnualPay()
+        {
+            return annualSalary += annualBonus;
+        }
+
+        public double HourlyPay()
+        {
+            return annualSalary / 1820;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace PayCalculator
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee e = new Employee("Jeff", 25000.00, 3000.00);
+            Console.WriteLine("Jeff's total annual pay is: \t" + e.TotalAnnualPay());
+            Console.WriteLine("His hourly rate is: \t\t" + e.HourlyPay());
+        }
+    }
+}
