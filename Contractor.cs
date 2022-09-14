b96c857999af60854616a5a6a4c2800dc06ffafc
SigherWolf <thomasw2701@gmail.com>
2022-09-14 17:41:50 +0100

    Started implementng contractor (temporary employee) class. It contains 2 fields, a constructor, and a 'TotalAnnualPay' method based on weeks worked annually

---
+++ Contractor.cs
@@ -0,0 +1,24 @@
+﻿namespace PayCalculator
+{
+    public class Contractor
+    {
+        double dayRate;
+        int weeksWorkedAnnually;
+
+        public Contractor(double dayRate, int weeksWorkedAnnually)
+        {
+            this.dayRate = dayRate;
+            this.weeksWorkedAnnually = weeksWorkedAnnually;
+        }
+
+        public double TotalAnnualPay()
+        {
+            return dayRate * weeksWorkedAnnually;
+        }
+
+        public double HourlyPay()
+        {
+
+        }
+    }
+}
