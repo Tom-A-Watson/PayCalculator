dc914a3b27a86714a849e3cb79b5c0854c0a0f1b
SigherWolf <thomasw2701@gmail.com>
2023-02-20 08:04:54 +0000

    Added unit tests for the mapper classes

---
+++ PayCalculator/PayCalculatorTests/PermanentEmployeeMapperTest.cs
@@ -0,0 +1,45 @@
+﻿using PayCalculatorAPI.Services;
+using PayCalculatorLibrary.Models;
+
+namespace PayCalculatorTest
+{
+    [TestFixture]
+    public class PermanentEmployeeMapperTest
+    {
+#nullable disable
+        private PermanentEmployeeMapper _mapper;
+        private CreateOrUpdatePermanentEmployee _model;
+        private const string ModelName = "william";
+        private const decimal ModelSalary = 40000;
+        private const decimal ModelBonus = 2000;
+        private const int ModelHoursWorked = 150;
+#nullable enable
+
+        [Test]
+        public void TestTemporaryEmployeeMapping()
+        {
+            // Arrange
+            _mapper = new();
+            _model = new()
+            {
+                Name = ModelName,
+                Salary = ModelSalary,
+                Bonus = ModelBonus,
+                HoursWorked = ModelHoursWorked
+            };
+
+            // Act
+            var _employee = _mapper.Map(_model);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_employee.GetType, Is.EqualTo(typeof(PermanentEmployee)));
+                Assert.That(_employee.Name, Is.EqualTo(ModelName));
+                Assert.That(_employee.Salary, Is.EqualTo(ModelSalary));
+                Assert.That(_employee.Bonus, Is.EqualTo(ModelBonus));
+                Assert.That(_employee.HoursWorked, Is.EqualTo(ModelHoursWorked));
+            });
+        }
+    }
+}
---
+++ PayCalculator/PayCalculatorTests/TemporaryEmployeeMapperTest.cs
@@ -0,0 +1,42 @@
+﻿using PayCalculatorAPI.Services;
+using PayCalculatorLibrary.Models;
+
+namespace PayCalculatorTest
+{
+    [TestFixture]
+    public class TemporaryEmployeeMapperTest
+    {
+#nullable disable
+        private TemporaryEmployeeMapper _mapper;
+        private CreateOrUpdateTemporaryEmployee _model;
+        private const string ModelName = "jonathan";
+        private const decimal ModelDayRate = 600;
+        private const int ModelWeeksWorked = 40;
+#nullable enable
+
+        [Test]
+        public void TestTemporaryEmployeeMapping()
+        {
+            // Arrange
+            _mapper = new();
+            _model = new()
+            {
+                Name = ModelName,
+                DayRate = ModelDayRate,
+                WeeksWorked = ModelWeeksWorked
+            };
+
+            // Act
+            var _employee = _mapper.Map(_model);
+
+            // Assert
+            Assert.Multiple(() =>
+            {
+                Assert.That(_employee.GetType, Is.EqualTo(typeof(TemporaryEmployee)));
+                Assert.That(_employee.Name, Is.EqualTo(ModelName));
+                Assert.That(_employee.DayRate, Is.EqualTo(ModelDayRate));
+                Assert.That(_employee.WeeksWorked, Is.EqualTo(ModelWeeksWorked));
+            });
+        }
+    }
+}
