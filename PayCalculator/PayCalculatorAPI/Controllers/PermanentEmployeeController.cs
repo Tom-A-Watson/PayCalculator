441aeda82a23ba3900ccf6219f5394a2558c93b9
SigherWolf <thomasw2701@gmail.com>
2023-03-09 15:50:51 +0000

    Made logging far more efficient by incorporating a 'LogMessagess' class, and inserting interpolated values at correct indexes in the messages. Also added an appender that allows logging in a rolling file

--- PayCalculator/PayCalculatorAPI/Controllers/PermanentEmployeeController.cs
+++ PayCalculator/PayCalculatorAPI/Controllers/PermanentEmployeeController.cs
@@ -29,10 +29,11 @@ namespace PayCalculatorAPI.Controllers
         public IActionResult Get()
         {
             var employeeList = _permEmployeeRepo.GetAll();
+            var employeeCount = employeeList.Count();
 
-            if (employeeList.Count() == 0)
+            if (employeeCount == 0)
             {
-                _log4net.Info("Returned an empty list of permanent employees");
+                _log4net.Info(LogMessages.EmptyEmployeeList.Insert(26, "permanent "));
                 return NotFound(ErrorMessages.EmptyEmployeeList);
             }
 
@@ -41,7 +42,7 @@ namespace PayCalculatorAPI.Controllers
                 employee.TotalAnnualPay = _permPayCalculator.TotalAnnualPay(employee.Salary, employee.Bonus);
             }
 
-            _log4net.Info($"Returned all permanent employees, {employeeList.Count()} were found");
+            _log4net.Info("Permanent " + LogMessages.ReturnedAllEmployees.Insert(24, Convert.ToString(employeeCount)));
             return Ok(employeeList);
         }
 
@@ -49,56 +50,62 @@ namespace PayCalculatorAPI.Controllers
         public IActionResult GetEmployee(int id)
         {
             var employee = _permEmployeeRepo.GetEmployee(id);
+            var stringID = Convert.ToString(id);
 
             if (employee == null)
             {
-                _log4net.Error($"Permanent employee with ID {id} does not exist!");
+                _log4net.Error("Permanent " + LogMessages.NullEmployee.Insert(17, stringID) + " Unable to read");
                 return NotFound(ErrorMessages.IdNotFound);
             }
 
             employee.TotalAnnualPay = _permPayCalculator.TotalAnnualPay(employee.Salary, employee.Bonus);
-            _log4net.Info($"Permanent employee with ID {id} read sucessfully");
+            _log4net.Info("Permanent " + LogMessages.EmployeeRead.Insert(17, stringID));
             return Ok(employee);
         }
 
         [HttpPut]
         public IActionResult Create(CreateOrUpdatePermanentEmployee createModel)
         {
-            var employee = _mapper.Map(createModel);
-            var newEmployee = _permEmployeeRepo.Create(employee);
-            newEmployee.TotalAnnualPay = _permPayCalculator.TotalAnnualPay(employee.Salary, employee.Bonus);
-            _log4net.Info($"Created a new permanent employee with an ID of {newEmployee.Id}");
-            return Created($"/permanentemployee/{newEmployee.Id}", newEmployee);
+            var mappedEmployee = _mapper.Map(createModel);
+            var employee = _permEmployeeRepo.Create(mappedEmployee);
+            employee.TotalAnnualPay = _permPayCalculator.TotalAnnualPay(mappedEmployee.Salary, mappedEmployee.Bonus);
+            _log4net.Info("Permanent " + LogMessages.EmployeeCreated + $"{employee.Id}");
+            return Created($"/permanentemployee/{employee.Id}", employee);
         }
 
         [HttpPatch("{id}")]
         public IActionResult Update(int id, [FromBody] CreateOrUpdatePermanentEmployee updateModel)
         {
+            var employee = _mapper.Map(updateModel);
+            var stringID = Convert.ToString(id);
+
             if (_permEmployeeRepo.GetEmployee(id) == null)
             {
-                _log4net.Error($"Permanent employee with ID {id} does not exist!");
+                _log4net.Error("Permanent " + LogMessages.NullEmployee.Insert(17, stringID) + " Unable to update");
                 return NotFound(ErrorMessages.IdNotFound);
             }
 
-            var employee = _mapper.Map(updateModel);
             employee.Id = id;
             _permEmployeeRepo.Update(employee);
-            _log4net.Info($"Permanent employee with ID {id} successfully updated");
+            _log4net.Info("Permanent " + LogMessages.EmployeeUpdated.Insert(17, stringID));
             return Ok();
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            var employeeName = _permEmployeeRepo.GetEmployee(id).Name;
             var delete = _permEmployeeRepo.Delete(id);
-
+            var stringID = Convert.ToString(id);
+            var deleteMessage = "Permanent " + LogMessages.EmployeeDeleted.Insert(17, stringID);
+            
             if (delete == false)
             {
-                _log4net.Error($"Permanent employee with ID {id} does not exist!");
+                _log4net.Error("Permanent " + LogMessages.NullEmployee.Insert(17, stringID) + " Unable to delete");
                 return NotFound(ErrorMessages.IdNotFound);
             }
 
-            _log4net.Info($"Permanent employee with ID {id} successfully deleted");
+            _log4net.Info(deleteMessage.Insert(29, $"({employeeName})"));
             return Ok(delete);
         }
     }
--- PayCalculator/PayCalculatorAPI/Controllers/TemporaryEmployeeController.cs
+++ PayCalculator/PayCalculatorAPI/Controllers/TemporaryEmployeeController.cs
@@ -1,10 +1,10 @@
-﻿using log4net;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PayCalculatorAPI.Services;
 using PayCalculatorLibrary.Constants;
 using PayCalculatorLibrary.Models;
 using PayCalculatorLibrary.Repositories;
 using PayCalculatorLibrary.Services;
+using log4net;
 
 namespace PayCalculatorAPI.Controllers
 {
@@ -14,7 +14,7 @@ namespace PayCalculatorAPI.Controllers
     {
         private readonly IEmployeeRepository<TemporaryEmployee> _tempEmployeeRepo;
         private readonly ITemporaryPayCalculator _tempPayCalculator;
-        private ITemporaryEmployeeMapper _mapper;
+        private readonly ITemporaryEmployeeMapper _mapper;
         private readonly ILog _log4net;
 
         public TemporaryEmployeeController(IEmployeeRepository<TemporaryEmployee> tempEmployeeRepo, ITemporaryPayCalculator tempPayCalculator, ITemporaryEmployeeMapper mapper)
@@ -22,17 +22,18 @@ namespace PayCalculatorAPI.Controllers
             _tempEmployeeRepo = tempEmployeeRepo;
             _tempPayCalculator = tempPayCalculator;
             _mapper = mapper;
-            _log4net = LogManager.GetLogger(typeof(PermanentEmployeeController));
+            _log4net = LogManager.GetLogger(typeof(TemporaryEmployeeController));
         }
 
         [HttpGet]
         public IActionResult Get()
         {
             var employeeList = _tempEmployeeRepo.GetAll();
+            var employeeCount = employeeList.Count();
 
-            if (employeeList.Count() == 0)
+            if (employeeCount == 0)
             {
-                _log4net.Info("Returned an empty list of temporary employees");
+                _log4net.Info(LogMessages.EmptyEmployeeList.Insert(26, "temporary "));
                 return NotFound(ErrorMessages.EmptyEmployeeList);
             }
 
@@ -41,7 +42,7 @@ namespace PayCalculatorAPI.Controllers
                 employee.TotalAnnualPay = _tempPayCalculator.TotalAnnualPay(employee.DayRate, employee.WeeksWorked);
             }
 
-            _log4net.Info($"Returned all temporary employees, {employeeList.Count()} were found");
+            _log4net.Info("Temporary " + LogMessages.ReturnedAllEmployees.Insert(24, Convert.ToString(employeeCount)));
             return Ok(employeeList);
         }
 
@@ -49,56 +50,62 @@ namespace PayCalculatorAPI.Controllers
         public IActionResult GetEmployee(int id)
         {
             var employee = _tempEmployeeRepo.GetEmployee(id);
-            
+            var stringID = Convert.ToString(id);
+
             if (employee == null)
             {
-                _log4net.Error($"Temporary employee with ID {id} does not exist!");
+                _log4net.Error("Temporary " + LogMessages.NullEmployee.Insert(17, stringID) + " Unable to read");
                 return NotFound(ErrorMessages.IdNotFound);
             }
 
             employee.TotalAnnualPay = _tempPayCalculator.TotalAnnualPay(employee.DayRate, employee.WeeksWorked);
-            _log4net.Info($"Temporary employee with ID {id} read successfully");
+            _log4net.Info("Temporary " + LogMessages.EmployeeRead.Insert(17, stringID));
             return Ok(employee);
         }
 
         [HttpPut]
         public IActionResult Create(CreateOrUpdateTemporaryEmployee createModel)
         {
-            var employee = _mapper.Map(createModel);
-            var newEmployee = _tempEmployeeRepo.Create(employee);
-            newEmployee.TotalAnnualPay = _tempPayCalculator.TotalAnnualPay(employee.DayRate, employee.WeeksWorked);
-            _log4net.Info($"Created a new temporary employee with an ID of {newEmployee.Id}");
-            return Created($"/temporaryemployee/{newEmployee.Id}", newEmployee);
+            var mappedEmployee = _mapper.Map(createModel);
+            var employee = _tempEmployeeRepo.Create(mappedEmployee);
+            employee.TotalAnnualPay = _tempPayCalculator.TotalAnnualPay(mappedEmployee.DayRate, mappedEmployee.WeeksWorked);
+            _log4net.Info("Temporary " + LogMessages.EmployeeCreated + $"{employee.Id}");
+            return Created($"/temporaryemployee/{employee.Id}", employee);
         }
 
         [HttpPatch("{id}")]
         public IActionResult Update(int id, [FromBody] CreateOrUpdateTemporaryEmployee updateModel)
         {
+            var employee = _mapper.Map(updateModel);
+            var stringID = Convert.ToString(id);
+
             if (_tempEmployeeRepo.GetEmployee(id) == null)
             {
-                _log4net.Error($"Temporary employee with ID {id} does not exist!");
+                _log4net.Error("Temporary " + LogMessages.NullEmployee.Insert(17, stringID) + " Unable to update");
                 return NotFound(ErrorMessages.IdNotFound);
             }
 
-            var employee = _mapper.Map(updateModel);
             employee.Id = id;
             _tempEmployeeRepo.Update(employee);
-            _log4net.Error($"Temporary employee with ID {id} successfully updated");
+            _log4net.Info("Temporary " + LogMessages.EmployeeUpdated.Insert(17, stringID));
             return Ok();
         }
 
         [HttpDelete("{id}")]
         public IActionResult Delete(int id)
         {
+            var employeeName = _tempEmployeeRepo.GetEmployee(id).Name;
             var delete = _tempEmployeeRepo.Delete(id);
+            var stringID = Convert.ToString(id);
+            var deleteMessage = "Temporary " + LogMessages.EmployeeDeleted.Insert(17, stringID);
 
             if (delete == false)
             {
-                _log4net.Error($"Temporary employee with ID {id} does not exist!");
+                _log4net.Error("Temporary " + LogMessages.NullEmployee.Insert(17, stringID) + " Unable to delete");
                 return NotFound(ErrorMessages.IdNotFound);
             }
 
-            _log4net.Info($"Temporary employee with ID {id} successfully deleted");
+            _log4net.Info(deleteMessage.Insert(29, $"({employeeName})"));
             return Ok(delete);
         }
     }
