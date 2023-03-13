c52ac7c5fe66468852c7edc3dfe2d558b090f405
SigherWolf <thomasw2701@gmail.com>
2023-03-13 16:21:19 +0000

    Successfully ran MVC and created a 'PermanentEmployee' page with an Index.cshtml file. This must be navigated to manually by adding '/PermanentEmployee' on the end of the localhost URL

---
+++ PayCalculator/PayCalculatorMVC/Controllers/PermanentEmployeeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PayCalculatorMVC.Controllers
+{
+    public class PermanentEmployeeController : Controller
+    {
+        private readonly ILogger<PermanentEmployeeController> _logger;
+
+        public PermanentEmployeeController(ILogger<PermanentEmployeeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public IActionResult GetAll()
+        //{
+
+        //}
+
+        //public IActionResult Create()
+        //{
+
+        //}
+
+        //public IActionResult GetEmployee()
+        //{
+
+        //}
+
+        //public IActionResult Update()
+        //{
+
+        //}
+
+        //public IActionResult Delete()
+        //{
+
+        //}
+    }
+}
