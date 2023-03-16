bfd961e726a3e678240e154233bfa74e63ab1887
SigherWolf <thomasw2701@gmail.com>
2023-03-16 09:46:18 +0000

    Started displaying all permanent employees from the list on the page. Styled this to display in a table format using bootstrap

--- PayCalculator/PayCalculatorMVC/Controllers/PermanentEmployeeController.cs
+++ PayCalculator/PayCalculatorMVC/Controllers/PermanentEmployeeController.cs
@@ -1,32 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using PayCalculatorLibrary.Models;
+using PayCalculatorLibrary.Repositories;
 
 namespace PayCalculatorMVC.Controllers
 {
     public class PermanentEmployeeController : Controller
     {
         private readonly ILogger<PermanentEmployeeController> _logger;
+        private readonly IEmployeeRepository<PermanentEmployee> _permEmployeeRepo;
 
-        public PermanentEmployeeController(ILogger<PermanentEmployeeController> logger)
+        public PermanentEmployeeController(ILogger<PermanentEmployeeController> logger, IEmployeeRepository<PermanentEmployee> permEmployeeRepo)
         {
             _logger = logger;
+            _permEmployeeRepo = permEmployeeRepo;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var employeeList = _permEmployeeRepo.GetAll();
+            return View(employeeList);
         }
 
-        //public IActionResult GetAll()
-        //{
-
-        //}
 
         //public IActionResult Create()
-        //{
-
-        //}
-
-        //public IActionResult GetEmployee()
         //{
 
         //}
--- PayCalculator/PayCalculatorMVC/Program.cs
+++ PayCalculator/PayCalculatorMVC/Program.cs
@@ -1,7 +1,11 @@
+using PayCalculatorLibrary.Models;
+using PayCalculatorLibrary.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddSingleton<IEmployeeRepository<PermanentEmployee>, PermanentEmployeeRepository>();
 
 var app = builder.Build();
 
