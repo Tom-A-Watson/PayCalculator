01eb2c628a1db0ad4cc5dfab453e7faf8db1e5c7
Tom Watson <Tom.Watson@tpr.gov.uk>
2023-06-13 13:51:29 +0100

    Initial commit for Entity Framework Core database. Successfully implemented primitive functionslity for adding and getting permanent employees. Added 'EFCoreConsole' console app for testing

---
+++ PayCalculator/EFCoreConsole/Program.cs
@@ -0,0 +1,37 @@
+﻿using PayCalculatorData;
+using PayCalculatorLibrary.Models;
+
+using (EmployeeContext context =  new EmployeeContext())
+{
+    context.Database.EnsureCreated();
+}
+
+GetPermanentEmployees();
+AddPermanentEmployee();
+GetPermanentEmployees();
+
+void AddPermanentEmployee()
+{
+    var employee = new PermanentEmployee()
+    {
+        Name = "Harry A",
+        Salary = 25000,
+        Bonus = 2000,
+        StartDate = new DateTime(2022, 8, 15)
+    };
+
+    using var context = new EmployeeContext();
+    context.PermanentEmployees.Add(employee);
+    context.SaveChanges();
+}
+
+void GetPermanentEmployees()
+{
+    using var context = new EmployeeContext();
+    var employees = context.PermanentEmployees.ToList();
+
+    foreach (var employee in employees)
+    {
+        Console.WriteLine(employee.Name);
+    }
+}
---
+++ PayCalculator/PayCalculatorData/Class1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PayCalculatorLibrary.Models;
+
+namespace PayCalculatorData
+{
+    public class EmployeeContext : DbContext
+    {
+        public DbSet<PermanentEmployee> PermanentEmployees { get; set; }
+        public DbSet<TemporaryEmployee> TemporaryEmployees { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = EmployeeDatabase"
+            );
+        }
+    }
+}
